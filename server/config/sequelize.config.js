8200aa5f2f20264c35877eb92135b01a9cd8f67a
John <johnpua@outlook.com>
2023-12-21 15:20:05 -0800

    adjustments

--- server/config/sequelize.config.js
+++ server/config/sequelize.config.js
@@ -1,10 +1,6 @@
 const { Sequelize } = require('sequelize');
 //... schema nae ideas, your user, your password
-<<<<<<< HEAD
-const sequelize = new Sequelize('ideas', 'root', 'Codewiser2543', {
-=======
-const sequelize = new Sequelize('ideas', 'root', 'imcnabb1234', {
->>>>>>> upstream/main
+const sequelize = new Sequelize('ideas', 'root', 'rootroot', {
 host: 'localhost',
 dialect: 'mysql'
 });
